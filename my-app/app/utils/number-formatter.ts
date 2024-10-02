517fe06a4cb58227a3e76608b629532c5d171dc0
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-10-02 11:39:31 +0530

    fix:delete folders

--- my-app/app/utils/number-formatter.ts
+++
@@ -1,8 +0,0 @@
-export const toIDRCurrency = (amount: number) => {
-    const formatter = new Intl.NumberFormat('id-ID', {
-      style: 'currency',
-      currency: 'IDR',
-    })
-  
-    return formatter.format(amount).toString().slice(0, -3)
-  }
